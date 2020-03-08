2f58c66fa95493d605d021b204218e22ff3bc06d
mikel-ocejo <mocejo004@gmail.com>
2020-03-08 17:55:57 +0100

    Ejercicio 1.1
    Primer ejercicio de OOP

---
+++ ejercicios/com/oop/Circulo.java
@@ -0,0 +1,43 @@
+package ejercicios.com.oop;
+/**
+ * La clase circulo representa un circulo con radio y color.
+ */
+
+public class Circulo {  // Guardar como"Circle.java"
+   // TODO Instancias de atributos privados del circulo, no accesibles desde fuera de la clase
+   
+   // TODO constructores (sobrecargados)
+   /** Constructor para crear una instancia de un circulo con el radio y el color por defecto */
+   public Circulo() {  
+   // TODO Definir atributos del para el constructor
+   }
+   
+   /** Constructor para crear una instancia de un circulo con un radio por parametro y el color por defecto */
+   public Circulo(double r) {  
+   // TODO Definir atributos del para el constructor
+   }
+   
+   /** Constructor para crear una instancia de un circulo con un radio y un color por parametro */
+   public Circulo(double r, String color) {  
+   // TODO Definir atributos del para el constructor
+   }
+
+   /** Getter del radio */
+   public double getRadio() {
+   // TODO devuelve el radio
+   }
+   
+   /** Getter del area del circulo*/
+   public double getArea() {
+   // TODO devuelve el area
+   }
+
+   /** Metodo para imprimir circulo */
+   public void imprimirDatosCirculo() {
+   /**  TODO imprime los circulos con el siguiente formato 
+   * <Identificador objeto> :
+   * Color: <color>
+   * Radio/Area: <radio>/<area>
+   **/
+   }
+}
---
+++ ejercicios/com/oop/circuloMain.java
@@ -0,0 +1,9 @@
+package ejercicios.com.oop;
+import ejercicios.com.oop.Circulo;
+
+class circuloMain{
+    public static void main(String[] args) {
+        //TODO prueba los diferentes constructores y metodos de la clase circulo
+    }
+}
+
